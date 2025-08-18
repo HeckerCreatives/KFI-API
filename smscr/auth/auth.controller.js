c6c4fae2173b23528d9c2f04e9ab885397edb0c2
ParadiseTeam69 <113586211+charl191992@users.noreply.github.com>
2025-08-18 15:05:04 +0800

    changes

--- smscr/auth/auth.controller.js
+++ smscr/auth/auth.controller.js
@@ -3,11 +3,15 @@ const { generateAccess } = require("../../configs/generate-token");
 
 exports.login = (req, res) => {
   passport.authenticate("local", (err, user, info) => {
-    console.log(err);
     if (err) return res.status(500).json({ success: false, msg: info, data: err });
     if (!user) return res.status(401).json({ success: false, msg: info.message });
     if (user.status === "banned" || user.status === "inactive") return res.status(403).json({ success: false, msg: "Account is either banned or inactive." });
     if (user.deletedAt) return res.status(403).json({ success: false, msg: "Invalid credentials" });
+
+    if (user.role !== "superadmin" && user.permissions.map(permission => permission.actions.visible).every(e => e === false)) {
+      return res.status(403).json({ success: false, msg: "Your account doesn't have an access to any page" });
+    }
+
     const access = generateAccess(user);
     return res.status(200).json({ success: true, access: access.access });
   })(req, res);
--- smscr/statistics/statistics.route.js
+++ smscr/statistics/statistics.route.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const statController = require("./statistics.controller.js");
+const { isAuthorize } = require("../../middlewares/authorized.js");
 
 const statsRoutes = express.Router();
 
 statsRoutes
-  .get("/cards", statController.getDashboardCardStatistics)
-  .get("/recent-members", statController.getRecentMembers)
-  .get("/recent-loans", statController.getRecentLoans)
-  .get("/loans-per-center", statController.getLoansPerCenter);
+  .get("/cards", isAuthorize("dashboard", "visible"), statController.getDashboardCardStatistics)
+  .get("/recent-members", isAuthorize("dashboard", "visible"), statController.getRecentMembers)
+  .get("/recent-loans", isAuthorize("dashboard", "visible"), statController.getRecentLoans)
+  .get("/loans-per-center", isAuthorize("dashboard", "visible"), statController.getLoansPerCenter);
 
 module.exports = statsRoutes;
