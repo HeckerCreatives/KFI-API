5b22691e49219f3303979abc21b5953a4e5db67d
ParadiseTeam69 <113586211+charl191992@users.noreply.github.com>
2025-09-29 08:35:00 +0800

    login username 3 characters

--- smscr/auth/auth.validation.js
+++ smscr/auth/auth.validation.js
@@ -2,7 +2,7 @@ const { body } = require("express-validator");
 const { platforms } = require("../../constants/platforms");
 
 exports.loginRules = [
-  body("username").trim().notEmpty().withMessage("Username is required").isLength({ min: 6, max: 30 }).withMessage("Username must consist of 6 to 30 characters"),
+  body("username").trim().notEmpty().withMessage("Username is required").isLength({ min: 3, max: 30 }).withMessage("Username must consist of 6 to 30 characters"),
   body("password").trim().notEmpty().withMessage("Password is required").isLength({ min: 6, max: 30 }).withMessage("Password must consist of 6 to 30 characters"),
   body("deviceName").trim().notEmpty().withMessage("Device name is required").isLength({ min: 6, max: 30 }).withMessage("Device name must consist of 6 to 30 characters"),
   body("deviceType")
